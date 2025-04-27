d0dc7d9c6b702f76dd80fef612cf51cfb7cb1787
Dustin Condon <dustin@debian.ipv6.telus.net>
2025-04-27 10:33:26 -0700

    Rename test file

--- 8086/unit_test.c
+++ 8086/unit_test.c
@@ -5,7 +5,7 @@ int main(int argc, char** arcv) {
   {
     printf("First line\n");
     u16 bin = 0b1000100111011001;
-    OpCode result = decode(bin);
+    Instruction result = decode(bin);
     Register expRegLeft = CX;
     Register expRegRight = BX;  
     assert(expRegLeft == result.Register2 && "Left register incorrect\n");
@@ -17,13 +17,16 @@ int main(int argc, char** arcv) {
   {
     printf("Second line\n");
     u16 bin = 0b1000100011100101;
-    OpCode result = decode(bin);
+    Instruction result = decode(bin);
     Register expRegLeft = CH;
     Register expRegRight = AH;  
     assert(expRegLeft == result.Register2 && "Left register incorrect\n");
     assert(expRegRight == result.Register1 && "Right register incorrect\n");
     print_opcode(&result);
+    printf("\n");
   }
+
+ 
   return 0;
 }
 
